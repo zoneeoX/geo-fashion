2c2a47076453358d46421cb831bf173d9adea97e
Raja Revanery Ramadhan <rajarevaneri@gmail.com>
2022-06-19 18:32:48 +0700

    homeanimation

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,87 @@ import About from "./Page/About/About";
 import Contacts from "./Page/Contacts";
 import Gallery from "./Page/Gallery";
 import Home from "./Page/Home";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import Geo from "./Images/geo.png";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [joke, setJoke] = useState("");
+
+  const candaanApi = async () => {
+    const option = {
+      url: "https://candaan-api.vercel.app/api/text/random",
+    };
+
+    try {
+      await axios.request(option).then((data) => {
+        const response = data;
+        setJoke(response.data.data);
+      });
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    candaanApi();
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 6000);
+  }, []);
+
   return (
     <>
+      <AnimatePresence>
+        {loading && (
+          <motion.div
+            className="w-screen h-screen flex justify-center items-center text-center fixed z-50"
+            initial={{y: '-100vw'}}
+            animate={{ y: 0 }}
+            exit={{ y: "200vw" }}
+            transition={{ duration: 1 }}
+          >
+            <motion.div
+              className="bg-rose-400 w-screen h-screen"
+              initial={{ y: "-100vw" }}
+              animate={{ y: 0 }}
+              transition={{ duration: 1 }}
+            >
+              <motion.div
+                className="bg-blue-400 w-screen h-screen"
+                initial={{ y: "-100vw" }}
+                animate={{ y: 0 }}
+                transition={{ duration: 1.5 }}
+              >
+                <motion.div
+                  className="bg-orange-400 w-screen h-screen"
+                  initial={{ y: "-100vw" }}
+                  animate={{ y: 0 }}
+                  transition={{ duration: 2 }}
+                >
+                  <motion.div
+                    className="bg-amber-900 w-screen h-screen flex justify-center items-center flex-col"
+                    initial={{ y: "-100vw" }}
+                    animate={{ y: 0 }}
+                    transition={{ duration: 2.5 }}
+                  >
+                    <img src={Geo} alt='geologo' className="w-40" />
+                    <h2 className="text-white text-lg md:text-2xl font-josef w-[30vw]">{joke}</h2>
+                  </motion.div>
+                </motion.div>
+              </motion.div>
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
       <Routers>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          {!loading && <Route path="/" element={<Home />} />}
           <Route path="/gallery" element={<Gallery />} />
           <Route path="/about" element={<About />} />
           <Route path="/contacts" element={<Contacts />} />
--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -8,38 +8,41 @@ import { useNavigate } from "react-router-dom";
 function Home() {
   const toGallery = useNavigate();
 
+  
   return (
     <>
-      <div>
-        <img
-          src={Sun}
-          alt="gambar"
-          className="w-screen h-screen object-center object-cover -z-10 fixed brightness-50"
-        />
-        <div className="w-screen h-screen flex items-center">
-          <motion.div
-            className="mx-[16vw] flex gap-6 flex-col"
-            initial={{ x: "-5vw", opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            transition={{ duration: 1.3 }}
-          >
-            <h1 className="sm:text-8xl text-6xl text-white font-paci flex flex-col w-40 gap-6">
-              Fashion Your
-              Clothing
-            </h1>
-            <motion.h2 className="text-white font-josef sm:text-2xl text-lg sm:w-72 w-50">
-              Wear something new, feels like brand new,
-              <span className="text-amber-600 font-bold"> Geo</span> got it all
-              for you.
-            </motion.h2>
-            <button className="bg-amber-600 hover:bg-amber-700 transition duration-200 font-josef rounded-full text-white px-4 py-1 text-xl w-fit" onClick={() => toGallery('/gallery')}>
-              Explore Gallery
-            </button>
-          </motion.div>
+        <div>
+          <img
+            src={Sun}
+            alt="gambar"
+            className="w-screen h-screen object-center object-cover -z-10 fixed brightness-50"
+          />
+          <div className="w-screen h-screen flex items-center">
+            <motion.div
+              className="mx-[16vw] flex gap-6 flex-col"
+              initial={{ x: "-5vw", opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1.3 }}
+            >
+              <h1 className="sm:text-8xl text-6xl text-white font-paci flex flex-col w-40 gap-6">
+                Fashion Your Clothing
+              </h1>
+              <motion.h2 className="text-white font-josef sm:text-2xl text-lg sm:w-72 w-50">
+                Wear something new, feels like brand new,
+                <span className="text-amber-600 font-bold"> Geo</span> got it
+                all for you.
+              </motion.h2>
+              <button
+                className="bg-amber-600 hover:bg-amber-700 transition duration-200 font-josef rounded-full text-white px-4 py-1 text-xl w-fit"
+                onClick={() => toGallery("/gallery")}
+              >
+                Explore Gallery
+              </button>
+            </motion.div>
+          </div>
+          <Summary />
+          <Footer />
         </div>
-        <Summary />
-        <Footer />
-      </div>
     </>
   );
 }
